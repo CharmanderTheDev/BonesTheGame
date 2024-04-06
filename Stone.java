f1330b269571c454d07799e70c5743b6df324f9b
CharmanderTheDev <cvmounce07@gmail.com>
2024-04-06 17:50:13 -0500

    added stone

---
+++ Stone.java
@@ -0,0 +1,21 @@
+import java.awt.*;
+
+public class Stone extends Ground{
+    
+    protected int fertility;
+    
+    public Stone(int height, int temperature, int weight, int lifeForce, int warp, int vapors, int sunlight, Coords coords, Chunk chunk){
+        super(height, temperature, weight, lifeForce, warp, vapors, sunlight, coords, chunk);
+    }
+
+    public char drawChar(){return('#');}
+    public Color drawColor(){return(new Color(150, 75, 0));}
+
+    public void tick(World world) {
+
+    }
+
+    public String inspect(int perception) {
+        return("");
+    }
+}
