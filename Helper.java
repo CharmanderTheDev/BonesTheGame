8fa8ebbb8cea0cfef90f687d1a20125ee82582bc
CharmanderTheDev <144705748+CharmanderTheDev@users.noreply.github.com>
2024-04-22 14:59:25 +0000

    modified helper, removing rendundant method

--- Helper.java
+++ Helper.java
@@ -1,11 +1,3 @@
 public class Helper {
 
-    public static int boolstoint(boolean[] bools){
-        int total = 0;
-        for(int i=0;i<bools.length;i++){
-            if(bools[i]){total+=Math.pow(2,i);}
-        }
-        return(total);
-    }
-
 }
